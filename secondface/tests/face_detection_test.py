017a6d29172c3757ed86e37ff9736385cb8ea77e
Vladislav <vladserkoff@protonmail.com>
2018-09-12 17:56:02 +0300

    face detection updated tests

--- secondface/tests/face_detection_test.py
+++ secondface/tests/face_detection_test.py
@@ -7,6 +7,7 @@ import os
 from PIL import Image
 
 from secondface.face_detection import FaceDetector
+from secondface.face import Face
 
 TEST_DIR_PATH = os.path.join(
     os.path.realpath(os.path.dirname(__file__)), 'data')
@@ -22,9 +23,24 @@ class Test:
         image = Image.open(TEST_IMG_PATH_FACES)
         faces = detector.find_faces(image)
         assert len(faces) == 3
+        face = faces[0]
+        assert isinstance(face, Face)
+        assert hasattr(face, 'box')
+        assert len(face.box) == 4
+        assert hasattr(face, 'image')
+        assert hasattr(face, 'confidence')
 
     def test_no_detection(self):
         detector = FaceDetector()
         image = Image.open(TEST_IMG_PATH_NO_FACES)
         faces = detector.find_faces(image)
         assert len(faces) == 0
+
+    def test_detection_batch(self):
+        detector = FaceDetector()
+        images = [
+            Image.open(x)
+            for x in [TEST_IMG_PATH_NO_FACES, TEST_IMG_PATH_FACES]
+        ]
+        faces = detector.find_faces_batch(images)
+        assert len(faces) == 2
