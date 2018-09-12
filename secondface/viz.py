58c86f91bfd478535c76925529355278963403c5
Vladislav <vladserkoff@protonmail.com>
2018-09-12 17:59:55 +0300

    Visualization utils

--- secondface/viz.py
+++ secondface/viz.py
@@ -3,12 +3,46 @@
 Visualization tools
 """
 
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple, Union
 
+import matplotlib.font_manager as fmgr
 import matplotlib.image as mimg
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
+
+def _get_default_font() -> ImageFont:
+    fm = fmgr.FontManager()
+    djv = [x for x in fm.ttffiles if x.endswith('DejaVuSans.ttf')][0]
+    font = ImageFont.truetype(djv, 18)
+    return font
+
+
+DEFAULT_FONT = _get_default_font()
+DEFAULT_RESIZE = (1024, 1024)
+
+
+def resize(image: Image.Image,
+           new_size: Tuple[int, int] = DEFAULT_RESIZE) -> Image.Image:
+    """
+    Fit image into new size, respecting the aspect ratio.
+
+    Parameters
+    ----------
+    image : `PIL.Image.Image`
+        Original image.
+    new_size : tuple
+        Resize image to fit into this box, (W x H)
+
+    Returns
+    -------
+    resize_image : `PIL.Image.Image`
+        Resized copy of the original image.
+    """
+    image = image.copy()
+    image.thumbnail(new_size)
+    return image
 
 
 def show_as_grid(images: List[Image.Image],
@@ -42,8 +76,8 @@ def show_as_grid(images: List[Image.Image],
     ymaps = int(np.ceil(nmaps / xmaps))
     height, width = int(array.shape[1] + padding), int(array.shape[2] +
                                                        padding)
-    grid = np.zeros(
-        (height * ymaps + padding, width * xmaps + padding, 3), dtype=np.uint8)
+    grid = np.zeros((height * ymaps + padding, width * xmaps + padding, 3),
+                    dtype=np.uint8)
     for y_coord in range(ymaps):
         for x_coord in range(xmaps):
             y_start, y_end = y_coord * height + padding, (y_coord + 1) * height
@@ -61,4 +95,54 @@ def show_as_grid(images: List[Image.Image],
         labelbottom=False,
         labelleft=False)
     image = ax.imshow(grid, interpolation='nearest')
+    return image
+
+
+def plot_boxes(image: Image.Image,
+               detected_faces: List[Dict[str, Any]]) -> Image.Image:
+    """
+    Plot original image with boxes around identified faces.
+
+    Parameters
+    ----------
+    image : `PIL.Image.Image` instance
+        Image used in `find_faces` step
+    detected_faces : list of dicts
+        Dictionaries with `box` fields for every face found on the `image`
+
+    Returns
+    -------
+    image_with_boxes : `PIL.Image.Image`
+        Original image with boxes drawn around identified faces.
+    """
+    image = image.copy()
+    draw = ImageDraw.Draw(image)
+    for face in detected_faces:
+        draw.rectangle(face['box'])
+    return image
+
+
+def overlay_text(image: Image,
+                 text: Union[str, float, int, np.float, np.int]) -> Image:
+    """
+    Draw a text in the top-left corner of the image.
+
+    Parameters
+    ----------
+    image : `PIL.Image.Image`
+        Original image.
+    text : str or float or int
+        Text to draw on the image. If float, will be rounded to two
+        decimal points.
+
+    Returns
+    -------
+    img : `PIL.Image.Image`
+        Copy of original image with overlayed text.
+    """
+    if isinstance(text, (float, np.floating)):
+        text = round(text, 2)
+    image = image.copy()
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), str(text), font=DEFAULT_FONT)
     return image
