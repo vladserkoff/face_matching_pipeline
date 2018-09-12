b6528774c37aaf8d8a40637350410e9e82b97e20
Vladislav <vladserkoff@protonmail.com>
2018-09-12 17:57:35 +0300

    face encoding support batching, rename test

--- secondface/face_encoding.py
+++ secondface/face_encoding.py
@@ -1,4 +1,4 @@
-# pylint: disable = R0903
+# pylint: disable = R0903, R0914
 """
 Face embedding based on original Tensorflow implementation of Facenet
 """
@@ -25,7 +25,9 @@ class FaceEncoder:
 
     def calculate_embeddings(self,
                              images: List[Image.Image],
-                             flip: bool = True) -> np.ndarray:
+                             flip: bool = True,
+                             batch_size: int = 128,
+                             report_progress: bool = True) -> np.ndarray:
         """
         Calculate face embeddings
 
@@ -36,6 +38,10 @@ class FaceEncoder:
         flip : bool, default True
             Weather to flip images and return average embeddings
             for flipped and non-flipped variants.
+        batch_size : int
+            Size of batch
+        report_progress : bool, default True
+            Whether to report on the number of images processed.
 
         Returns
         -------
@@ -48,7 +54,24 @@ class FaceEncoder:
             flipped_np = np.stack([np.array(x) for x in images_flip])
             images_np = np.concatenate([images_np, flipped_np])
         images_np = (images_np - 127.5) / 128.0
-        embeddings = self.facenet.forward(images_np)
+
+        n_images = images_np.shape[0]
+        n_batches = int(np.ceil(n_images / batch_size))
+        embeddings_all = []
+        batch_start = 0
+        for i in range(n_batches):
+            batch_end = batch_start + batch_size
+            batch = images_np[batch_start:batch_end]
+            embeddings_batch = self.facenet.forward(batch)
+            embeddings_all.append(embeddings_batch)
+            batch_start = batch_end
+            if report_progress:
+                print(
+                    '\r{} of {} batches done'.format(i + 1, n_batches), end='')
+        if report_progress:
+            print()
+        embeddings = np.concatenate(embeddings_all, axis=0)
+
         if flip:
             new_shape = (-1, len(images), embeddings.shape[-1])
             embeddings = embeddings.reshape(new_shape)
--- secondface/tests/face_encoding_test.py
+++ secondface/tests/face_encoding_test.py
@@ -3,22 +3,25 @@
 Tests that all faces are detected, and processed as expected
 """
 import os
-import numpy as np
 
-from secondface.data import load_images
+import numpy as np
+from PIL import Image
+
 from secondface.face_encoding import FaceEncoder
 
 TEST_DIR_PATH = os.path.join(
     os.path.realpath(os.path.dirname(__file__)), 'data')
-TEST_IMGS_PAT = os.path.join(TEST_DIR_PATH, 'face*.jpg')
-
-IMAGES = load_images(TEST_IMGS_PAT)
+TEST_IMGS = [
+    os.path.join(TEST_DIR_PATH, x)
+    for x in ['face0.jpg', 'face1.jpg', 'face2.jpg']
+]
 
 
 class Test:
     def test_encoding(self):
         encoder = FaceEncoder()
-        embeddings = encoder.calculate_embeddings(IMAGES)
+        images = [Image.open(x) for x in TEST_IMGS]
+        embeddings = encoder.calculate_embeddings(images)
         assert isinstance(embeddings, np.ndarray)
         assert embeddings.shape == (3, 512)
         assert np.allclose(np.linalg.norm(embeddings, axis=1), 1)
