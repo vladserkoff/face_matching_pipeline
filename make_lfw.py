33855d05c64d9ec4bd477469a9d2e9802be99d05
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 10:05:42 +0000

    Adding tarfile member sanitization to extractall()

--- make_lfw.py
+++ make_lfw.py
@@ -83,7 +83,26 @@ def prepare_lfw_data(lfw_dir: str) -> Tuple[str, str]:
         path = fetch_remote_lfw()
         lfw_dir = os.path.abspath(lfw_dir)
         with tarfile.open(path) as archive:
-            archive.extractall(lfw_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, lfw_dir)
         os.remove(path)
 
     references_dir = os.path.join(lfw_dir, 'references')
